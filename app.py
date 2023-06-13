44215a344c571abcfd8b09e7f379df00c4abde3a
ipmanlk <io@navinda.xyz>
2023-06-13 20:51:43 +0530

    root commit

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, request
+import pickle
+import pandas as pd
+from pathlib import Path
+
+from generate_model import  generate_model, get_label_encoder
+
+# paths
+MODELS_DIR = Path(__file__).parent / "models"
+
+# Initialize model
+generate_model()
+
+# Load general model
+with open(MODELS_DIR / 'model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+app = Flask(__name__)
+
+user_data = {
+    "123" : {
+        "GuardianPhoneNumber": "+94714445555",
+        "Velocity": 0.0,
+        "Lean": 0.0,
+        "Pitch": 0.0,
+        "Yaw": 0.0,
+        "Longitude": 6.9271,
+        "Latitude": 79.8612,
+        "Status": "Not Crashed"
+    },
+}
+
+# POST request
+@app.route('/store-user-data', methods=['POST'])
+def store_user_data():
+    data = request.get_json()
+    uid = str(data['uid'])
+    user_data[uid]["Velocity"] = float(data["Velocity"])
+    user_data[uid]["Lean"] = float(data["Lean"])
+    user_data[uid]["Pitch"] = float(data["Pitch"])
+    user_data[uid]["Yaw"] = float(data["Yaw"])
+    user_data[uid]["Longitude"] = str(data["Longitude"])
+    user_data[uid]["Latitude"] = str(data["Latitude"])
+
+
+    # Predict
+    X = pd.DataFrame({
+        "Velocity": [user_data[uid]["Velocity"]],
+        "Lean": [user_data[uid]["Lean"]],
+        "Pitch": [user_data[uid]["Pitch"]],
+        "Yaw": [user_data[uid]["Yaw"]],
+    })
+
+    prediction = model.predict(X)
+    label_encoder = get_label_encoder()
+    predicted_category = label_encoder.inverse_transform(prediction)[0]
+    user_data[uid]["Status"] = predicted_category
+
+    return user_data[uid]
+
+# GET request
+@app.route('/get-user-data', methods=['GET'])
+def get_user_data():
+    uid = str(request.args.get('uid'))
+    return user_data[uid]
+
+
+app.run(debug=True)
---
+++ generate_model.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import pickle
+from pathlib import Path
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+
+DATASET_PATH = Path(__file__).parent / "csv" / "dataset.csv"
+MODEL_PATH = Path(__file__).parent / "models" / "model.pkl"
+
+def generate_model():
+  # Load the dataset
+  data = pd.read_csv(DATASET_PATH)
+  data.columns = data.columns.str.strip()
+
+  # Separate features and target variable
+  X = data.drop('Status', axis=1)
+  y = data['Status']
+
+  # Perform label encoding on the target variable
+  le = LabelEncoder()
+  y = le.fit_transform(y)
+
+  # Split the dataset into training and testing sets
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+  # Train the DecisionTreeClassifier
+  classifier = DecisionTreeClassifier()
+  classifier.fit(X_train, y_train)
+
+  # Export the trained model as a pickle file
+  with open(MODEL_PATH, 'wb') as f:
+    pickle.dump(classifier, f)
+
+  print("Model has been generated successfully.")
+
+def get_label_encoder():
+    # Load the dataset
+  data = pd.read_csv(DATASET_PATH)
+  data.columns = data.columns.str.strip()
+
+  # Separate features and target variable
+  y = data['Status']
+
+  # Perform label encoding on the target variable
+  le = LabelEncoder()
+  y = le.fit_transform(y)
+
+  return le
